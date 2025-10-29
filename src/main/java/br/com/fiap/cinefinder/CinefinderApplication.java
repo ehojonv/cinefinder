2701c4b05b45731b3b3dc94d8fb8cc09500d2511
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-10-29 00:16:57 -0300

    Initialize Cinefinder project with Maven wrapper, configuration files, and basic Spring Boot application structure

---
+++ src/main/java/br/com/fiap/cinefinder/CinefinderApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cinefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinefinderApplication.class, args);
+	}
+
+}
